cb6de97c8e598e92f3d9aaa84a7b133a2f601f37
Zack Kollar <rallokkcaz@gmail.com>
2019-01-26 04:42:42 -0800

    Added terrible migrations

--- priv/migrations/20190126033938065_alter_user.cr
+++ priv/migrations/20190126033938065_alter_user.cr
@@ -1,9 +1,15 @@
 class AlterUser < Jennifer::Migration::Base
   def up
-    # TODO: DO SOMETHING HERE
+    change_table(:users) do |t|
+      t.drop_column(:password_hash)
+      t.add_column(:password_digest, :string, { default: ""})
+    end
   end
 
   def down
-    #TODO: REVERT IT BACK TO THE ORIGINAL
+    change_table(:users) do |t|
+      t.drop_column(:password_digest)
+      t.add_column(:password_hash, :string, { default: "" })
+    end
   end
 end
